4c170e87dd8e24576afa22936fe73b1937d317b4
onlyoneprivate <pllm098@naver.com>
2025-03-27 19:25:47 +0900

    feat : 회원가입

--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=15, blank = True, null = True)
+
+    groups = models.ManyToManyField(Group, related_name = "customer_set", blank = True)
+    user_permissions = models.ManyToManyField(Permission, related_name="customer_permission_set", blank=True)
