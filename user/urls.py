9318c36e4c36c0550be35bb20e5d79aecb1b9611
onlyoneprivate <pllm098@naver.com>
2025-04-05 23:22:15 +0900

    feat : 비밀번호 재설정 템플릿 오버라이딩 구현

--- user/urls.py
+++ user/urls.py
@@ -12,9 +12,11 @@ urlpatterns = [
     path('change_password/', change_password_view, name='change_password'),
 
     # 비밀번호 재설정
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), 
+    # 템플릿 오버라이딩 추가
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), 
          name='password_reset_confirm'),
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
+    
 ]
